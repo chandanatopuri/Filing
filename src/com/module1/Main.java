7b1f1c3f091958ce2285fdf944776eb2a48866ab
chandanatopuri <chandanatopuri@gmail.com>
2022-03-24 19:45:39 +0530

    added filehandling

---
+++ src/com/module1/Main.java
@@ -0,0 +1,13 @@
+package com.module1;
+
+public class Main {
+
+	/*Enter your desired Directory path */
+	public static final String path = "C:/Users/Directory"; 
+	
+	public static void main(String[] args) {
+		Menus menu = new Menus();
+		menu.introScreen();
+		menu.mainMenu();
+	}
+}
